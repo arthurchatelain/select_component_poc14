22a2875b932f9975ca1d045ca4505f588e18647b
Arthur Chatelain <arthur.chatelain.off@gmail.com>
2022-07-05 03:11:03 +0200

    onClickOutside

--- src/lib/components/Select.js
+++ src/lib/components/Select.js
@@ -1,11 +1,16 @@
 import { useState } from "react"
 import PropTypes from 'prop-types'
 import React from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 /**
  * A select component to choose between different possibilities.
+ * example : 
+ *  ```js
+ * <Select onChange={e => {}} data={[''value1', 'value2', 'value3']} title='Department'/>
+ * ```
  * @prop {string} title - The select title that will be used as the default value
- * @prop {func} OnChange - Function to call when the user select a new possibility 
+ * @prop {func} onChange - Function to call when the user select a new possibility 
  * @prop {array} data - An array of string with the different possibilities
  */
 
@@ -23,24 +28,26 @@ export default function Select (props) {
     const ArrowBottom = () => <svg viewBox="0 0 330 330"><path d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393  c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393  s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/></svg> 
 
     const handleClick = e => { // e is the new value
+        let value = e.target.dataset.value
         setIsOpen(false)
-        setValue(e)
-        props.OnChange(e) 
+        setValue(value)
+        props.onChange(value) 
     }
 
     return (
-        <div className="Select">
+        <div className="Select" >
             { 
                 !isOpen ? <div className="selectCell" onClick={() => setIsOpen(true)}><p>{value}</p><ArrowBottom /></div>
-                : <div className="selectContainer">
+                : <OutsideClickHandler onOutsideClick={() => setIsOpen(false)}><div className="selectContainer">
                     {
                         props.data.map(item => 
-                            <div key={item} className="selectCells" onClick={e => {handleClick(e.target.firstChild.textContent)}}>
-                                <p>{item}</p>
+                            <div key={item} data-value={item}
+                             className="selectCells" onClick={e => {handleClick(e)}}>
+                                {item}
                             </div>
                         )
                     }
-                </div> 
+                </div></OutsideClickHandler>
             }
         </div>
     ) 
